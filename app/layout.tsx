7f479810b284a8756150dd014445b0a843423d48
dkiriinya <dkiriinya06sc@gmail.com>
2025-07-26 23:47:10 +0300

    added favicon details and seo details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,9 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter, Poppins , Work_Sans , DM_Sans} from "next/font/google"
+import { DM_Sans} from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "../components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
-})
-const workSans = Work_Sans({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-work-sans",
-  display: "swap",
-  fallback: ["sans-serif"],
-  style: ["normal", "italic"],
-})
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -29,7 +15,7 @@ const dmSans = DM_Sans({
 })
 
 export const metadata: Metadata = {
-  title: "Ethics Journey Portfolio",
+  title: "Don Gitonga's Ethical Journey",
   description: "A personal reflection on 12 weeks of exploring moral philosophy and ethical decision-making",
 }
 
@@ -40,6 +26,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Don's ethical Journey" />
+      </head>
       <body className={dmSans.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
